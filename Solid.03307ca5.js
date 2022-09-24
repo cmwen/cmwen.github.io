2d73a336262c632a4cf20d85724632a3d1482aef
cmwen <cmwen@users.noreply.github.com>
2022-09-24 12:14:36 +0000

    deploy: 438ac7f53dded2a7964ef0c7c7de3387b32785c2

---
+++ Solid.03307ca5.js
@@ -0,0 +1,1 @@
+import{d as o,c as s,g as a,i as c,r as d,t as u}from"./chunks/web.1f7f4acf.js";const l=u('<button type="button" class="rounded-md border border-sky-500 w-24 hover:border-sky-600 shadow-md"></button>');function m(){const[e,n]=s(0),r=()=>n(e()+1);return(()=>{const t=a(l);return t.$$click=r,c(t,e),d(),t})()}o(["click"]);export{m as default};
