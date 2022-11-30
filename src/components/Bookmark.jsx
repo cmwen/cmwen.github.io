5d9c6aa16c28e39a06cb2236a3636b095908f091
ChungMin Wen <chungmin.wen@gmail.com>
2022-11-30 21:31:14 +1100

    feat: add bookmark filter

--- src/components/Bookmark.jsx
+++ src/components/Bookmark.jsx
@@ -1,9 +1,9 @@
 
 export const Bookmark = ({name, url, description}) => {
   return (
-    <div class="border rounded p-2 max-w-xs h-24 hover:animate-pulse">
-        <a href={url} class="text-sky-300 text-lg ">{name}</a>
-        <div class="text-xs">{description}</div>
+    <div class="border rounded p-3 max-w-xs h-24 hover:animate-pulse">
+        <a href={url} class="text-sky-300 text-lg block">{name}</a>
+        <span class="text-xs">{description}</span>
     </div>
   )
 }
--- src/components/Bookmarks.jsx
+++ src/components/Bookmarks.jsx
@@ -1,11 +1,47 @@
+import { createSignal } from "solid-js";
 import Bookmark from "./Bookmark";
+import Dropdown from "./Dropdown";
 
-export const Bookmarks = ({bookmarks}) => {
-    return <div class="flex flex-row space-x-4">
-        {
-            bookmarks.map(bookmark => <Bookmark {...bookmark} />)
-        }
-    </div>
-}
+const populateTags = (bookmarks) => {
+  const tagSet = new Set();
+
+  bookmarks
+    .map((bookmark) => bookmark.tags)
+    .forEach((tags) => {
+      tags.forEach((tag) => tagSet.add(tag));
+    });
+
+  return [...tagSet.values()];
+};
+
+export const Bookmarks = ({ bookmarks }) => {
+  const tags = populateTags(bookmarks);
+  const [tagFilter, setTagFilter] = createSignal("");
+
+  console.log(tagFilter());
+  const onFilterChange = (env) => {
+    setTagFilter(env.target.value);
+  };
+
+  return (
+    <>
+      <Dropdown
+        label="Tags"
+        options={["", ...tags]}
+        onChange={onFilterChange}
+      />
+      <div class="flex flex-wrap flex-row space-x-4">
+        {bookmarks
+          .filter(
+            (bookmark) =>
+              tagFilter() === "" || bookmark.tags.includes(tagFilter())
+          )
+          .map((bookmark) => (
+            <Bookmark {...bookmark} />
+          ))}
+      </div>
+    </>
+  );
+};
 
 export default Bookmarks;
